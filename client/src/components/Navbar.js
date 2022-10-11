bb71366db585d046dcbee8e509e044dc1922b896
puritymwaura <purity.mwaura@student.moringaschool.com>
2022-10-11 07:58:30 +0300

    created a navbar

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,25 @@
+function Nav(){
+    return(
+        <>
+
+        <nav className="navbar navbar-light bg-light">
+        <div className="container-fluid">
+        <a className="navbar-brand" href="vg">
+        <img
+        src="https://mdbcdn.b-cdn.net/img/logo/mdb-transaprent-noshadows.webp"
+        className="me-2"
+        height="20"
+        alt="MDB Logo"
+        loading="lazy"
+         />
+        <small>MDBootstrap</small>
+    </a>
+    </div>
+    </nav>
+        </>
+        
+    )
+  
+  }
+  
+  export default Nav
