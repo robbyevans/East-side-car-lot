6ca2a58dce41dc241a4419366a52e8afdcaff601
puritymwaura <purity.mwaura@student.moringaschool.com>
2022-10-11 07:58:51 +0300

    created a footer

---
+++ client/src/components/Footer.js
@@ -0,0 +1,12 @@
+function Footer(){
+    return (
+        <>
+        <footer className="bg-dark text-center text-white">
+        <div className="text-center p-3" style={{backgroundColor: "rgba(0, 0, 0, 0.2)"}}>
+            <a className="text-white" href="#icescreamers.com/">© 2022 Copyright: Ice Screamers</a>
+        </div>
+        </footer>
+        </>
+    )
+}
+export default Footer
